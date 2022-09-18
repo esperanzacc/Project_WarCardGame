2f848fb2f3fce476531217567cb54f4006ae50d0
esperanzacc <v.esperanzac113@gmail.com>
2022-09-18 12:10:19 -0700

    Fix the code of updating score

--- WarCardGame/ContentView.swift
+++ WarCardGame/ContentView.swift
@@ -8,45 +8,79 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-      VStack {
-        Spacer()
-        Image("logo")
-        Spacer()
-        HStack(spacing: 80.0) {
-          Image("card3")
-            .cornerRadius(10.0)
-          Image("card4")
-            .cornerRadius(10.0)
-        }
-        Spacer()
-          
-        Image("dealbutton")
-        Spacer()
-        VStack {
-          HStack(spacing: 20.0) {
-            Text("Player")
-              
-              .font(.title)
-            Text("CPU")
   
-              .font(.title)
+  // State -> 1. change the data 2.get notify when data changed
+  
+  @State private var playerCard = "card5"
+  @State private var cpuCard = "card9"
+  @State private var playerScore = 0
+  @State private var cpuScore = 0
+  
+    var body: some View {
+      ZStack {
+        Image("background").ignoresSafeArea()
+        VStack {
+          Spacer()
+          Image("logo")
+          Spacer()
+          HStack {
+            Spacer()
+            Image(playerCard)
+              .cornerRadius(10.0)
+            Spacer()
+            Image(cpuCard)
+              .cornerRadius(10.0)
+            Spacer()
           }
-          HStack(spacing: 20.0) {
-            Text("0")
-        
-            Text("0")
+          Spacer()
+          Button {
             
+            // Generate a random number between 2 and 14
+            let playerRand = Int.random(in: 2...14)
+            let cpuRand = Int.random(in: 2...14)
+            // Update the cards
+            playerCard = "card" + String(playerRand)
+            cpuCard = "card" + String(cpuRand)
+            
+            // Update the score
+            if playerRand > cpuRand {
+              playerScore += 1
+            } else if cpuRand > playerRand {
+              cpuScore += 1
+            }
+          } label: {
+            Image("dealbutton")
           }
-
+          
+          Spacer()
+          
+          HStack {
+            Spacer()
+            VStack {
+              Text("Player")
+                .font(.headline)
+                .foregroundColor(Color.white)
+                .padding(.bottom, 15.0)
+              Text(String(playerScore))
+                .font(.largeTitle)
+                .foregroundColor(Color.white)
+            }
+            Spacer()
+            VStack {
+              Text("CPU")
+                .font(.headline)
+                .foregroundColor(Color.white)
+                .padding(.bottom, 14.0)
+              Text(String(cpuScore))
+                .font(.largeTitle)
+                .foregroundColor(Color.white)
+            }
+            Spacer()
+          }
+          Spacer()
         }
-        Spacer()
-        
         
       }
-      .background(Color.green)
-      .foregroundColor(Color.white)
-      .padding(.all)
        
 
     }
